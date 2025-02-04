02799edb03408b49a3d8a227c61e83b3595782b4
Rafael Farias <farifran@gmail.com>
2025-02-04 19:40:18 +0000

    js add

--- exercises/logica-js-projeto_inicial/app.js
+++ exercises/logica-js-projeto_inicial/app.js
@@ -1,1 +1,13 @@
-alert('prova');
+alert('Boas vindas ao nosso site');
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas =  50;
+let saldoDisponivel = 1000;
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+nome = prompt('Escreva seu nome');
+idade = prompt('Escreva sua idade');
+if(idade >= 18){
+    alert('Pode tirar a Habilitação');
+}
