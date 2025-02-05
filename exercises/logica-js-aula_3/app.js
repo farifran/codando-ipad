d0e35d007843bb810c81d73c7f1dfb90471f40b9
Rafael Farias <farifran@gmail.com>
2025-02-05 19:38:36 +0000

    Novo exercício

--- exercises/logica-js-aula_3/app.js
+++ exercises/logica-js-aula_3/app.js
@@ -1,5 +1,5 @@
-let diaSemana = prompt('Qual é o dia da semana?').toLowerCase();
-console.log(diaSemana);
+let diaSemana = prompt('Qual é o dia da semana?').toLowerCase().trim();
+console.log('Dia escolhido:', diaSemana);
 
 const fimDeSemana = ['sábado', 'sabado', 'domingo', '7', '1'];
 
@@ -8,3 +8,11 @@ if (fimDeSemana.includes(diaSemana)) {
 } else {
   alert('Boa semana!');
 }
+
+let numero = prompt('Escolha um número positivo ou negativo');
+if (numero > 0) {
+  alert('Positivo');
+} else {
+  alert('Negativo');
+}
+
