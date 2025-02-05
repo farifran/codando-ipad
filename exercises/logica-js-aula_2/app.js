7225f01b20915e1deb2dc1f6a69d1d8b5ef7dddd
Rafael Farias <farifran@gmail.com>
2025-02-05 16:48:56 +0000

    test

--- exercises/logica-js-aula_2/app.js
+++ exercises/logica-js-aula_2/app.js
@@ -3,8 +3,8 @@ let numeroSecreto = 5;
 console.log('Valor numero secreto:', numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 console.log('Valor chute:', chute);
+console.log('Resultado da comparacao:', chute == numeroSecreto);
 if (chute == numeroSecreto) {
-    console.log('Resultado da comparacao:', chute == numeroSecreto);
     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
     alert('Você errou :(');
