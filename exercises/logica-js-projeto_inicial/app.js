9a39152d591afa9f1bb5217a234680f80f08d221
Rafael Farias <farifran@gmail.com>
2025-02-05 00:15:23 +0000

    melhoras no código Javascript

--- exercises/logica-js-projeto_inicial/app.js
+++ exercises/logica-js-projeto_inicial/app.js
@@ -1,13 +1,14 @@
 alert('Boas vindas ao nosso site');
 let nome = 'Lua';
 let idade = 25;
-let numeroDeVendas =  50;
+let numeroDeVendas = 50;
 let saldoDisponivel = 1000;
-alert('Erro! Preencha todos os campos');
-let mensagemDeErro = 'Erro! Preencha todos os campos';
+const mensagemDeErro = 'Erro! Preencha todos os campos';
 alert(mensagemDeErro);
+
 nome = prompt('Escreva seu nome');
 idade = prompt('Escreva sua idade');
-if(idade >= 18){
+
+if (idade >= 18) {
     alert('Pode tirar a Habilitação');
 }
