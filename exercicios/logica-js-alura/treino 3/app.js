f25c7c67753e3a860d6cb628318124720279d3dd
Rafael Farias <farifran@gmail.com>
2025-02-07 15:08:47 -0300

    js treino 3

---
+++ exercicios/logica-js-alura/treino 3/app.js
@@ -0,0 +1,9 @@
+//Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
+//Crie uma variável chamada "nome" e atribua a ela o seu nome.Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!".
+//Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?.Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+//Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha.Em seguida, realize a soma desses dois valores e armazene o resultado em ……m loop while para imprimir os números de 1 a 10 no console.
+//Crie uma variável "nota" e atribua um valor numérico a ela.Use if-else para determinar se a nota é maior ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console.
+//Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
+//Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
+//Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
+
