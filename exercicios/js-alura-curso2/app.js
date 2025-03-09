d5ff7d59dd8e23dd7108c2d6de0afc325e5ab756
Rafael Farias <farifran@gmail.com>
2025-03-09 09:58:47 +0000

    funçoes

--- exercicios/js-alura-curso2/app.js
+++ exercicios/js-alura-curso2/app.js
@@ -1,9 +1,29 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+let numeroSecreto = gerarNumeroAleatorio();
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Jogo do número secreto';
+
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+
+function exibirNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    //if(texto === undefined) {
+    //    console.log('sem texto na tag', tag);
+    //}
+}  
+
+exibirNaTela('h1', 'Jogo do número secreto');
+exibirNaTela('p', 'Escolha um número entre 1 e 10');
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
 
 function verificarChute() {
-    console.log('O botão foi clicado');
+    let chute = document.querySelector('input').value;
+    console.log(numeroSecreto == chute);
 }
+
